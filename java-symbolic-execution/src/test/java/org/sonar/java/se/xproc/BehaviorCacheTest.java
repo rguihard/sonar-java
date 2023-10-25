41f524db9b2d428eee9baabcd087f9c50a7d45f6
Rguihard <remy.guihard@gmail.com>
2023-10-25 13:14:54 +0200

    Slower is better
    Delete rule for inexisting apache commons3 method
    Add unit test for the new rules

--- java-symbolic-execution/src/test/java/org/sonar/java/se/xproc/BehaviorCacheTest.java
+++ java-symbolic-execution/src/test/java/org/sonar/java/se/xproc/BehaviorCacheTest.java
@@ -198,8 +198,8 @@ class BehaviorCacheTest {
     }
 
     assertThat(behaviorCache.behaviors).isEmpty();
-    assertThat(behaviorCache.hardcodedBehaviors()).hasSize(233);
-    assertThat(logTester.logs(Level.DEBUG)).containsOnly("[SE] Loaded 233 hardcoded method behaviors.");
+    assertThat(behaviorCache.hardcodedBehaviors()).hasSize(236);
+    assertThat(logTester.logs(Level.DEBUG)).containsOnly("[SE] Loaded 236 hardcoded method behaviors.");
   }
 
   @Test
